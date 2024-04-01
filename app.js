d80049e749aa672e65560218eda1dd8795113884
Muhammad Avicena <cenarahmant.dev@gmail.com>
2024-04-01 12:31:36 +0700

    feat: add db conn

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import useMiddleware from "./middleware/index.js";
+import { dbConn } from "./config/dbConfig.js";
 import routes from "./router/index.js";
 import HttpErrorsNotFound from "./utils/http-errors/not-found-error.js";
 import errorHandlerMiddleware from "./middleware/error-handler.middleware.js";
@@ -9,6 +10,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// Db connection
+dbConn();
 
 // Use middleware
 useMiddleware(app);
