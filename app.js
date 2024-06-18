a8137bc78d67e287abf33183d023ce07e00cfbaa
Muhammad Avicena <cenarahmant.dev@gmail.com>
2024-06-18 22:14:58 +0700

    fix: database remove disconnect

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import useMiddleware from "./middleware/index.js";
-import { dbConn } from "./config/dbConfig.js";
+import dbConn from "./config/dbConfig.js";
 import routes from "./router/index.js";
 import HttpErrorsNotFound from "./utils/http-errors/not-found-error.js";
 import errorHandlerMiddleware from "./middleware/error-handler.middleware.js";
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Db connection
-dbConn();
+dbConn;
 
 // Use middleware
 useMiddleware(app);
